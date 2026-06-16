a45dcd57297619d492ecbdfddb40c082d4380e2b
Daniel Cabana <daniel.cabana@tecsup.edu.pe>
2026-06-15 19:22:52 -0500

    Creación del proyecto Maven

---
+++ Proyecto-final-poo/src/test/java/xyz/tecsup/proyectoFinal/Proyecto_final_poo/AppTest.java
@@ -0,0 +1,19 @@
+package xyz.tecsup.proyectoFinal.Proyecto_final_poo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
